00d6d1e0796a78c12539859621be8afd5e85efb3
JessiP23 <jessi316866@gmail.com>
2024-10-17 12:21:55 -0400

    phone

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -78,7 +78,7 @@ const ContactUsPage = () => {
             </button>
           </form>
           <div className="mt-8 flex flex-col space-y-2">
-            <div className="flex items-center">
+            <div className="flex items-center text-green-800">
               <Phone className="w-5 h-5 mr-2 text-purple-500" />
               <span>+1 (555) 123-4567</span>
             </div>
