b68a14f5fd71673963679f5022a6f5a7f8140351
JessiP23 <jessi316866@gmail.com>
2024-10-23 13:09:45 -0400

    loading

--- app/room-generation/page.js
+++ app/room-generation/page.js
@@ -629,7 +629,11 @@ export default function CustomizableRoom() {
 
   
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+      </div>
+    )
   }
 
   if (!user) {
