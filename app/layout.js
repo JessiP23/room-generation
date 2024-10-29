b14d3361ad6936fe4dbe1edb971fc0e4a430c496
JessiP23 <jessi316866@gmail.com>
2024-10-29 12:57:35 -0400

    new icon

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import myIcon from '../public/3d.png'
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,11 +23,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/3d.png" type="image/png"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
         <Analytics />
+        <div style={{ position: 'fixed', bottom: '10px', right: '10px' }}>
+          <Image 
+            src={myIcon} 
+            alt="Custom Analytics Icon" 
+            width={40} 
+            height={40} 
+          />
+
+        </div>
       </body>
     </html>
   );
